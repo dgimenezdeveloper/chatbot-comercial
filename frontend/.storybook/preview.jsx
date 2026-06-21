d3c3fd8548a60c4fc2d1a45533ccea921174d98d
manfredcamacho <manfredcb@gmail.com>
2026-06-21 05:56:16 -0300

    refactor: remove storybook addons: vitest and onbording. Add storybook vite aliases config

--- frontend/.storybook/preview.jsx
+++ frontend/.storybook/preview.jsx
@@ -1,17 +1,19 @@
 /** @type { import('@storybook/nextjs-vite').Preview } */
+import "../src/app/globals.css";
+
 const preview = {
   parameters: {
+    backgrounds: {
+      default: "light",
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+      disableSaveFromUI: true,
     },
-
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
       test: "todo",
     },
   },
