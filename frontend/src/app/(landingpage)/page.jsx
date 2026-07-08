6924bc282eb059c9859dadbeb52a8bdac8647185
Manfred Camacho <12779378+manfredcamacho@users.noreply.github.com>
2026-07-08 14:28:47 -0300

    [FRONT-001] autenticacion con google (#48)
    * chore: update .env.example
    
    * feat: allow google domain images
    
    * feat: add dependencies Next Auth for login and Pino for logging
    
    * feat: add Pino logger config
    
    * feat: add google authentication
    
    * feat: add dockerfile for frontend
    
    * feat: add frontend service to docker-compose
    
    * feat: add google authentication button component
    
    * feat: add basic layout to the app
    
    * feat: convert login form in component
    
    * feat: add google button stories
    
    * chore: delete old page stories
    
    * fix: add pnp-workspace allow buils for production image
    
    * refactor: change folder name to landingpage
    
    * chore: docker dev mode for frontend
    
    * refactor: change files and folders to kebab-case
    
    * update: modify README.md
    
    * fix: file capitalization

---
+++ frontend/src/app/(landingpage)/page.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "@/components/landing-page/hero-section/hero-section";
+import FeaturesSection from "@/components/landing-page/features-section/features-section";
+import OnboardingSection from "@/components/landing-page/onboarding-section/onboarding-section";
+import HowToSection from "@/components/landing-page/how-to-section/how-to-section";
+import ContactSection from "@/components/landing-page/contact-section/contact-section";
+
+export default function Page() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <OnboardingSection />
+      <HowToSection />
+      <ContactSection />
+    </>
+  );
+}
