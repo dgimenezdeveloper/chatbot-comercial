397cedaea96fb0817f0959a761520f61ddc74ee0
Roberto Rossa <rcrossa@hotmail.com>
2026-07-08 20:27:50 -0300

    Removidos duplicados y casing generado por sistemas macos.

--- frontend/src/components/landingpage/ContactSection/ContactSection.jsx
+++
@@ -1,3 +0,0 @@
-export default function ContactSection() {
-  return <section className="bg-gray-100 py-12 mx-auto text-center">Contact Section Content</section>
-}
--- frontend/src/components/layout/footer/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <footer className="bg-gray-800 text-white py-4 text-center"> Footer Content </footer>
-}
